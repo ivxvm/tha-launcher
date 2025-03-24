6c2fc38549e15de65cf0701cdbf8b5a042cbecff
ivxvm <ivxvm@protonmail.com>
2025-03-24 13:40:54 +0200

    Refactoring & updates

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use std::process::Command;
 
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
+const BLENDERPLAYER_PATH: &str = "C:\\Programs\\upbge-0.36.1-windows-x86_64\\blenderplayer.exe";
 
-    ui.on_request_exit({
-        || {
-            std::process::exit(0);
-        }
+fn main() -> Result<(), Box<dyn Error>> {
+    let app_window = AppWindow::new()?;
+    let app_window_weak = app_window.as_weak();
+
+    app_window.on_request_exit(|| {
+        std::process::exit(0);
     });
 
-    ui.run()?;
+    app_window.on_request_play_level(move || {
+        let app_window = app_window_weak.unwrap();
+        let mut cmd = Command::new(BLENDERPLAYER_PATH);
+        cmd.arg(app_window.get_level_path()).spawn().unwrap();
+    });
 
+    app_window.run()?;
     Ok(())
 }
