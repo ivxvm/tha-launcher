b98f8550c726c7fbc4fa5f5644ad2f12cafe9aff
ivxvm <ivxvm@protonmail.com>
2025-03-26 18:08:09 +0200

    Parse game stats, implement gallery tab

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,62 @@ struct AppConfig {
 
 const APP_NAME: &str = "xawn";
 const BLENDERPLAYER_PATH: &str = "C:\\Programs\\upbge-0.36.1-windows-x86_64\\blenderplayer.exe";
+const GAME_STATS_PATH: &str = "E:\\Blender\\Nodot\\gamestats.txt";
+
+fn parse_game_stats() -> Result<Vec<slint_generatedAppWindow::GameStats>, Box<dyn Error>> {
+    let mut game_stats = Vec::new();
+
+    for level_stats_str in std::fs::read_to_string(GAME_STATS_PATH)?.lines() {
+        if let [general_stats_str, unlocked_drawings_str, ..] =
+            level_stats_str.split(";").collect::<Vec<&str>>()[..]
+        {
+            if let [time_str, gems_str, total_gems_str, drawings_str, total_drawings_str, ..] =
+                general_stats_str.split(",").collect::<Vec<&str>>()[..]
+            {
+                // Parse general stats values
+                let time: i32 = time_str.parse()?;
+                let gems: i32 = gems_str.parse()?;
+                let total_gems: i32 = total_gems_str.parse()?;
+                let drawings: i32 = drawings_str.parse()?;
+                let total_drawings: i32 = total_drawings_str.parse()?;
+
+                // Parse unlocked drawing IDs into a boolean vector
+                let mut unlocked_drawings = vec![false; 3];
+                for drawing_id in unlocked_drawings_str
+                    .split(",")
+                    .filter_map(|id| id.parse::<usize>().ok())
+                {
+                    if drawing_id < 3 {
+                        unlocked_drawings[drawing_id] = true;
+                    }
+                }
+
+                // Create a GameStats instance for the level
+                game_stats.push(GameStats {
+                    time,
+                    gems,
+                    total_gems,
+                    drawings,
+                    total_drawings,
+                    unlocked_drawings: slint::ModelRc::new(slint::VecModel::from(
+                        unlocked_drawings,
+                    )),
+                });
+            }
+        }
+    }
+
+    Ok(game_stats)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut cfg: AppConfig = confy::load(APP_NAME, None)?;
+    let game_stats = parse_game_stats()?;
 
     let app_window = AppWindow::new()?;
     app_window.set_is_fullscreen(cfg.is_fullscreen);
     app_window.set_should_show_fps(cfg.should_show_fps);
+    app_window.set_game_stats(slint::ModelRc::new(slint::VecModel::from(game_stats)));
 
     let cfg_update_timer = Timer::default();
     cfg_update_timer.start(
@@ -63,6 +112,14 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .args(cmd_args)
                 .spawn()
                 .unwrap();
+        }
+    });
+
+    app_window.on_request_open_url({
+        let app_window_weak = app_window.as_weak();
+        move || {
+            let app_window = app_window_weak.unwrap();
+            open::that(app_window.get_url_to_open()).unwrap();
         }
     });
 
