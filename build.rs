5d29469730cbb7d94107c1d52348a83248ffcbe7
ivxvm <ivxvm@protonmail.com>
2025-03-28 16:53:26 +0200

    Move images to the launcher repo

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() {
 
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("E:\\Blender\\Nodot\\images\\icon.ico"); // Path to your .ico file
+        res.set_icon("./images/icon.ico"); // Path to your .ico file
         res.compile().expect("Failed to embed icon");
     }
 }
